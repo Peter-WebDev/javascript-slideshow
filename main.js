bd0cbe9a9bf5681d427c8f611d7ad814a4a376c9
Peter <peter.warholm@gmail.com>
2024-11-05 15:53:53 +0100

    Added javascript for slideshow, initial variable as zero as the first image, function for main, function for change visible image

---
+++ main.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded", main);
+
+//** Variable to check visibility of image */
+let visibleIndex = 0;
+
+//** Set up initial function */
+function main() {
+    setInterval(changeVisibleImageInSlidwShow, 3000);
+}
+
+//** Change Visible Image In Slideshow */
+function changeVisibleImageInSlidwShow() {
+    const images = document.querySelector(".slideshow-container").children; //Look at the children
+    images[visibleIndex].classList.remove("visible");
+    visibleIndex += 1;
+    if(visibleIndex >= images.length) {
+        visibleIndex = 0;
+    }
+    images[visibleIndex].classList.add("visible");
+}

e85c8bda2c2b71a961b118270f7752bc60aeb777
Peter <peter.warholm@gmail.com>
2024-11-06 21:18:18 +0100

    Added Toggle function, animated hamburger, added styles for the button

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ let visibleIndex = 0;
 
 //** Set up initial function */
 function main() {
-    setInterval(changeVisibleImageInSlidwShow, 5000);
+    setInterval(changeVisibleImageInSlideShow, 5000);
+    menuButton.onclick = toggleMenuOpened;
 }
 
 //** Change Visible Image In Slideshow */
-function changeVisibleImageInSlidwShow() {
+function changeVisibleImageInSlideShow() {
     const images = document.querySelector(".slideshow-container").children; //Look at the children
     images[visibleIndex].classList.remove("visible");
     visibleIndex += 1;
@@ -18,3 +19,11 @@ function changeVisibleImageInSlidwShow() {
     }
     images[visibleIndex].classList.add("visible");
 }
+
+//** Check if menu is opened */
+function toggleMenuOpened() {
+    const header = document.getElementById("menuHeader");
+    const nav = document.querySelector(".nav__primary");
+    header.classList.toggle("opened");
+    nav.classList.toggle("menuvisible");
+ }
